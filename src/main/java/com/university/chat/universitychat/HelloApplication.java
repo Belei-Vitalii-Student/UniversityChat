fe22ce1d5a9f4f0c52fd6a90ea972ae3ad22fa60
vitPrograms <vitalii.beley@gmail.com>
2023-11-27 10:41:27 +0200

    login page

--- src/main/java/com/university/chat/universitychat/HelloApplication.java
+++ src/main/java/com/university/chat/universitychat/HelloApplication.java
@@ -10,10 +10,16 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        scene.getStylesheets().add("bootstrap3.css");
+
+        stage.setTitle("University hub");
         stage.setScene(scene);
+
+        stage.setResizable(false);
+
         stage.show();
     }
 
