2302f9f00db11b49157eae8fd9fa5b8bb5f4c707
classuser200 <classuser200@local.iful.edu.ua>
2023-11-28 11:44:12 +0200

    click functionality

--- src/main/java/com/university/chat/universitychat/HelloController.java
+++ src/main/java/com/university/chat/universitychat/HelloController.java
@@ -1,14 +1,103 @@
 package com.university.chat.universitychat;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    Button log_in_btn;
+    @FXML
+    Button news_btn;
+    @FXML
+    Button schedule_btn;
+    @FXML
+    Button meeting_btn;
+    @FXML
+    Button new_meet_btn;
+    @FXML
+    GridPane news_view;
+    @FXML
+    TableView schedule_view;
+    @FXML
+    VBox meeting_view;
+    @FXML
+    Pane new_meet_form;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    MenuButton menu_options;
+    private Stage stage;
+    @FXML
+    protected void login(ActionEvent event) throws IOException {
+        Parent teacherView = FXMLLoader.load(getClass().getResource("teacher-view.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(teacherView);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    protected void logout(ActionEvent event) throws IOException {
+        Parent teacherView = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
+        stage = (Stage) menu_options.getScene().getWindow();
+        Scene scene = new Scene(teacherView);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    protected void selectView(ActionEvent event) {
+        Node node = (Node) event.getSource() ;
+        String data = (String) node.getUserData();
+
+        switch (data) {
+            case "news":
+                selectNewsView();
+                break;
+            case "schedule":
+                selectScheduleView();
+                break;
+            case "meeting":
+                selectMeetingView();
+        }
+    }
+
+    @FXML
+    protected void showMeetingForm() {
+        new_meet_form.setVisible(true);
+    }
+
+    @FXML
+    protected void hideMeetingForm() {
+        new_meet_form.setVisible(false);
+    }
+
+
+    private void selectNewsView() {
+        schedule_view.setVisible(false);
+        meeting_view.setVisible(false);
+        news_view.setVisible(true);
+    }
+
+    private void selectScheduleView() {
+        meeting_view.setVisible(false);
+        news_view.setVisible(false);
+        schedule_view.setVisible(true);
+    }
+
+    private void selectMeetingView() {
+        schedule_view.setVisible(false);
+        news_view.setVisible(false);
+        meeting_view.setVisible(true);
     }
 }
--- src/main/java/com/university/chat/universitychat/PostgresqlDB.java
+++ src/main/java/com/university/chat/universitychat/PostgresqlDB.java
@@ -46,102 +46,102 @@ public class PostgresqlDB {
         return userExists;
     }
 
-    public void addUser(String name, Integer age, String favoriteColor) throws SQLException {
-
-        String sql = "INSERT INTO users (name, age, favorite_color) VALUES (?, ?, ?);";
-//        String sql = "INSERT INTO users (name, age, favorite_color) VALUES ('" + name + "'," + age + ",'" + favoriteColor + "');";
-        PreparedStatement prepare = connection.prepareStatement(sql);
-
-        prepare.setString(1, name);
-        prepare.setInt(2, age);
-        prepare.setString(3, favoriteColor);
-
-        prepare.executeUpdate();
-        prepare.close();
-
-        logger.log("ADD", "Added new user [" + name + "]");
-    }
-
-    public void changeUserName(Integer id, String name) throws Exception {
-        if(!userExists(id)) {
-            throw new Exception("User with id-" + id + " doesn`t found.");
-        }
-
-        String sql = "UPDATE users SET name = ? WHERE id = ?;";
-        PreparedStatement prepare = connection.prepareStatement(sql);
-
-        prepare.setString(1, name);
-        prepare.setInt(2, id);
-
-        prepare.executeUpdate();
-        prepare.close();
-
-        logger.log("UPDATE", "Updated name for user [" + id + "] to [" + name + "]");
-    }
-
-    public void changeUserAge(Integer id, Integer age) throws Exception {
-        if(!userExists(id)) {
-            throw new Exception("User with id-" + id + " doesn`t found.");
-        }
-
-        String sql = "UPDATE users SET age = ? WHERE id = ?;";
-        PreparedStatement prepare = connection.prepareStatement(sql);
-
-        prepare.setInt(1, age);
-        prepare.setInt(2, id);
-
-        prepare.executeUpdate();
-        prepare.close();
-
-        logger.log("UPDATE", "Updated age for user [" + id + "] to [" + age + "]");
-    }
-
-    public void changeUserFavoriteColor(Integer id, String favoriteColor) throws Exception {
-        if(!userExists(id)) {
-            throw new Exception("User with id-" + id + " doesn`t found.");
-        }
-
-        String sql = "UPDATE users SET favorite_color = ? WHERE id = ?;";
-        PreparedStatement prepare = connection.prepareStatement(sql);
-
-        prepare.setString(1, favoriteColor);
-        prepare.setInt(2, id);
-
-        prepare.executeUpdate();
-        prepare.close();
-
-        logger.log("UPDATE", "Updated favorite color for user [" + id + "] to [" + favoriteColor + "]");
-    }
-
-    public void deleteUser(Integer id) throws Exception {
-        if(!userExists(id)) {
-            throw new Exception("User with id-" + id + " doesn`t found.");
-        }
-
-        String sql = "DELETE FROM users WHERE id = ?;";
-        PreparedStatement prepare = connection.prepareStatement(sql);
-
-        prepare.setInt(1, id);
-
-        prepare.executeUpdate();
-        prepare.close();
-
-        logger.log("DELETE", "Deleted user [" + id + "]");
-    }
-
-    public List<User> getAllUsers() throws SQLException {
-        List<User> users = new ArrayList();
-
-        Statement statement = connection.createStatement();
-        String sql = "SELECT * FROM users";
-        ResultSet set = statement.executeQuery(sql);
-        while (set.next()) {
-            users.add(new User(set.getInt("id"), set.getString("name"), set.getInt("age"), set.getString("favorite_color")));
-        }
-        statement.close();
-
-        return users;
-    }
+//    public void addUser(String name, Integer age, String favoriteColor) throws SQLException {
+//
+//        String sql = "INSERT INTO users (name, age, favorite_color) VALUES (?, ?, ?);";
+////        String sql = "INSERT INTO users (name, age, favorite_color) VALUES ('" + name + "'," + age + ",'" + favoriteColor + "');";
+//        PreparedStatement prepare = connection.prepareStatement(sql);
+//
+//        prepare.setString(1, name);
+//        prepare.setInt(2, age);
+//        prepare.setString(3, favoriteColor);
+//
+//        prepare.executeUpdate();
+//        prepare.close();
+//
+//        logger.log("ADD", "Added new user [" + name + "]");
+//    }
+//
+//    public void changeUserName(Integer id, String name) throws Exception {
+//        if(!userExists(id)) {
+//            throw new Exception("User with id-" + id + " doesn`t found.");
+//        }
+//
+//        String sql = "UPDATE users SET name = ? WHERE id = ?;";
+//        PreparedStatement prepare = connection.prepareStatement(sql);
+//
+//        prepare.setString(1, name);
+//        prepare.setInt(2, id);
+//
+//        prepare.executeUpdate();
+//        prepare.close();
+//
+//        logger.log("UPDATE", "Updated name for user [" + id + "] to [" + name + "]");
+//    }
+//
+//    public void changeUserAge(Integer id, Integer age) throws Exception {
+//        if(!userExists(id)) {
+//            throw new Exception("User with id-" + id + " doesn`t found.");
+//        }
+//
+//        String sql = "UPDATE users SET age = ? WHERE id = ?;";
+//        PreparedStatement prepare = connection.prepareStatement(sql);
+//
+//        prepare.setInt(1, age);
+//        prepare.setInt(2, id);
+//
+//        prepare.executeUpdate();
+//        prepare.close();
+//
+//        logger.log("UPDATE", "Updated age for user [" + id + "] to [" + age + "]");
+//    }
+//
+//    public void changeUserFavoriteColor(Integer id, String favoriteColor) throws Exception {
+//        if(!userExists(id)) {
+//            throw new Exception("User with id-" + id + " doesn`t found.");
+//        }
+//
+//        String sql = "UPDATE users SET favorite_color = ? WHERE id = ?;";
+//        PreparedStatement prepare = connection.prepareStatement(sql);
+//
+//        prepare.setString(1, favoriteColor);
+//        prepare.setInt(2, id);
+//
+//        prepare.executeUpdate();
+//        prepare.close();
+//
+//        logger.log("UPDATE", "Updated favorite color for user [" + id + "] to [" + favoriteColor + "]");
+//    }
+//
+//    public void deleteUser(Integer id) throws Exception {
+//        if(!userExists(id)) {
+//            throw new Exception("User with id-" + id + " doesn`t found.");
+//        }
+//
+//        String sql = "DELETE FROM users WHERE id = ?;";
+//        PreparedStatement prepare = connection.prepareStatement(sql);
+//
+//        prepare.setInt(1, id);
+//
+//        prepare.executeUpdate();
+//        prepare.close();
+//
+//        logger.log("DELETE", "Deleted user [" + id + "]");
+//    }
+//
+//    public List<User> getAllUsers() throws SQLException {
+//        List<User> users = new ArrayList();
+//
+//        Statement statement = connection.createStatement();
+//        String sql = "SELECT * FROM users";
+//        ResultSet set = statement.executeQuery(sql);
+//        while (set.next()) {
+//            users.add(new User(set.getInt("id"), set.getString("name"), set.getInt("age"), set.getString("favorite_color")));
+//        }
+//        statement.close();
+//
+//        return users;
+//    }
 
     public List<Log> getLogs() throws SQLException {
         List<Log> logs = new ArrayList();
